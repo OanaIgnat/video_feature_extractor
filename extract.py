33640cc30d85e6329c7c5746c5bdbd520054ece3
OanaIgnat <oignat@umich.edu>
2020-03-25 17:40:48 -0400

    preprocessing.py

--- extract.py
+++ extract.py
@@ -62,8 +62,8 @@ with th.no_grad():
 
                 if args.only_preprocess:
                     print("Saving only the preprocessed video")
-                    video = video.cpu().numpy()
                     normalized_video = F.normalize(video, dim=1)
+                    normalized_video = normalized_video.cpu().numpy()
                     if args.half_precision:
                         normalized_video = normalized_video.astype('float16')
                     np.save(output_file, normalized_video)
