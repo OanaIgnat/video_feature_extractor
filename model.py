edf81ee3be686efa59f5a8635edd1bf4dfcd651c
OanaIgnat <oignat@umich.edu>
2020-04-03 17:50:19 -0400

    test s3d

--- model.py
+++ model.py
@@ -37,6 +37,7 @@ def get_model(args):
     else:
         print('Loading S3D ...')
         model = S3D(
+            'model/s3d_dict.npy',
             num_classes=512
          )
         model = model.cuda()
